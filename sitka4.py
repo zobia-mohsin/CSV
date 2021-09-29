3cc838dbc6a6913f8745affb9152d6f7934db859
Zobia Mohsin <zobia_mohsin1@baylor.edu>
2021-09-29 15:01:46 -0500

    Step 4 done commit changes

--- sitka4.py
+++ sitka4.py
@@ -2,13 +2,13 @@ import matplotlib.pyplot as plt  # to get graph plt name of graph
 import csv
 from datetime import datetime
 
-open_file = open("sitka_weather_2018_simple.csv", "r")
+open_file = open("death_valley_2018_simple.csv", "r")
 
 csv_file = csv.reader(open_file, delimiter=',')
 
 header_row = next(csv_file)
 
-print(type(header_row))
+# print(type(header_row))
 
 for index, column_header in enumerate(header_row):
     print(index, column_header)
@@ -18,22 +18,36 @@ dates = []
 lows = []
 # testing the datetime strptime function
 mydate = datetime.strptime('2018-07-01', '%Y-%m-%d')
-print(mydate)
-print(type(mydate))  # to convert test to date use datetime
+# print(mydate)
+# print(type(mydate))  # to convert test to date use datetime
 
 for rec in csv_file:
-    lows.append(int(rec[6]))
-    highs.append(int(rec[5]))
-    the_date = datetime.strptime(rec[2], '%Y-%m-%d')
-    dates.append(the_date)
+
+    try:
+        the_date = datetime.strptime(rec[2], '%Y-%m-%d')
+        high = int(rec[4])  # to account for the missing data in line 50
+        low = int(rec[5])  # changed the index based on death valley file
+
+    except ValueError:  # only print this if error with data
+        # variable name in the string literal with the curly brackets
+        print(f'Missing data for {the_date}')
+    else:
+        highs.append(high)
+        lows.append(low)
+        dates.append(the_date)
+
+    # highs.append(int(rec[4]))
+    # lows.append(int(rec[5]))
+    #the_date = datetime.strptime(rec[2], '%Y-%m-%d')
+    # dates.append(the_date)
 
 
-print(highs)
-print(dates)
+# print(highs)
+# print(dates)
 
 
 fig = plt.figure()
-plt.title("Daily High Temperatures, July 2018", fontsize=16)
+plt.title("Daily High and Low Temperatures -2018\nDeath Valley", fontsize=16)
 plt.xlabel("", fontsize=12)
 plt.ylabel("Temperature (F)", fontsize=12)
 plt.tick_params(axis='both', which='both', labelsize=12)
